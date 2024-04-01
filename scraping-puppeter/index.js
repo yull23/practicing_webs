f8de0c2efbb6df575318c2094050afc99b741b8c
Yull Timoteo <yull.timoteo.z@uni.pe>
2024-03-31 20:25:27 -0500

    initial commit

---
+++ scraping-puppeter/index.js
@@ -0,0 +1,10 @@
+import puppeteer from "puppeteer";
+
+async function openWebPage() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto("https://developer.chrome.com/");
+}
+
+openWebPage();
